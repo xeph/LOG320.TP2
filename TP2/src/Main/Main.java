c31bbe002241dbdf3d2b45ca5b47e63cc64e6a12
Marc-Andre Destrempes <xeph_555@hotmail.com>
2013-06-06 19:51:36 -0400

    Correction du main et ajout de sudoku.

--- TP2/src/Main/Main.java
+++ TP2/src/Main/Main.java
@@ -4,36 +4,32 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length == 2) {
-			if (args[0] != "" || args[1] != "") {
+			if (!args[0].equals("") || !args[1].equals("")) {
 				int[][] grid = new int[9][9];
 				grid = reader.SudokuReader.readFile(args[0]);
 				
-				if (args[1] == "-o") { // version optimise
+				if (args[1].equals("-o")) { // version optimise
 					matrix.SudokuSparseMatrix ssm = new matrix.SudokuSparseMatrix(grid);
 					ssm.solve();
-				} else if (args[1] == "-n") // version non optimise
+				} else if (args[1].equals("-n")) // version non optimise
 					new solver.SudokuSolver().solve(grid);
 				else { // help
-					System.out.println("Sudoku solver v1.0");
-					System.out.println("par Samuel Beauchemin");
-					System.out.println("Marc-Andre Destrempes");
-					System.out.println("\nUsage:");
-					System.out.println("\t<path>\tChemin du fichier");
-					System.out.println("\t-o\tSolver optimise");
-					System.out.println("\t-n\tSolver non optimise");
-					System.out.println("\nSudokuSolver.jar <path> <-o|-n>");
+					help();
 				}
 			}
 		} else { // help
-			System.out.println("Sudoku solver v1.0");
-			System.out.println("Samuel Beauchemin");
-			System.out.println("Marc-Andre Destrempes");
-			System.out.println("\nUsage:");
-			System.out.println("\t<path>\tChemin du fichier");
-			System.out.println("\t-o\tSolver optimise");
-			System.out.println("\t-n\tSolver non optimise");
-			System.out.println("\njava -jar SudokuSolver.jar <path> <-o|-n>");
+			help();
 		}
 	}
 
+	private final static void help() {
+		System.out.println("Sudoku solver v1.0");
+		System.out.println("Samuel Beauchemin");
+		System.out.println("Marc-Andre Destrempes");
+		System.out.println("\nUsage:");
+		System.out.println("\t<path>\tChemin du fichier");
+		System.out.println("\t-o\tSolver optimise");
+		System.out.println("\t-n\tSolver non optimise");
+		System.out.println("\njava -jar SudokuSolver.jar <path> <-o|-n>");
+	}
 }
